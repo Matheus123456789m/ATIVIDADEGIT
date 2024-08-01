4031d1d08ca684cbe7ffa793cdc79a7f703a6bed
Matheus <josematheus1202@gmail.com>
2024-08-01 16:28:19 -0300

    Versao alterada

---
+++ ATIVIDADEGIT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATIVIDADEGIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
